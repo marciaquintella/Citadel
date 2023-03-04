3a6e34d1d1a67f034208e83f2706fa5c1e975e98
Nilce Moscone <nilcemayumi@gmail.com>
2023-03-04 17:10:37 -0300

    seed created

--- config/initializers/algoliasearch.rb
+++ config/initializers/algoliasearch.rb
@@ -1,2 +1,2 @@
 AlgoliaSearch.configuration = { application_id: 'R6R7DNKPN0',
-                                api_key: '' }
+                                api_key: ENV['ALGOLIA_KEY'] }
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-
+require "csv"
 User.destroy_all
 
 User.create!(email: "marciaquintella@gmail.com", password: "123456", admin: true)
@@ -33,10 +33,19 @@ User.create!(email: "nilcemayumi@gmail.com", password: "123456", admin: true)
 
 
 
+Function.destroy_all
 Language.destroy_all
 
 ruby = Language.create!(name: "Ruby", version: "3.1", source_reference: "	https://rubyapi.org/3.1")
 
-Function.destroy_all
+filepath = "db/function_seed.csv"
 
-Function.create!
+CSV.foreach(filepath, headers: :first_row) do |row|
+  function = Function.new(function_name: row['function_name'],
+                          content: row['content'],
+                          code: row['code'],
+                          klass: row['klass'],
+                          key_words: row['key_words'])
+  function.language = ruby
+  function.save
+end
