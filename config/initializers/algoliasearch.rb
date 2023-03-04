c098afe91a6c2cb7c09462ee65e02c5d365c8557
Nilce Moscone <nilcemayumi@gmail.com>
2023-03-04 15:32:52 -0300

    key removed

--- config/initializers/algoliasearch.rb
+++ config/initializers/algoliasearch.rb
@@ -1,2 +1,2 @@
 AlgoliaSearch.configuration = { application_id: 'R6R7DNKPN0',
-                                api_key: 'b322de61043d3dadfe3ff60961669ebe' }
+                                api_key: '' }
