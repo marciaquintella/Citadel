740cc6ddfa82702c137dce3f52e6c73661fa39fe
Gabriel Martins <gabriel.st.martins@gmail.com>
2023-03-09 20:16:23 -0300

    Home complete

---
+++ app/controllers/payments_controller.rb
@@ -0,0 +1,6 @@
+class PaymentsController < ApplicationController
+  skip_before_action :authenticate_user!
+  def new
+    @order = Order.find(params[:order_id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   root to: "pages#home"
+  resources :donations, only: [:index, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  
+  resources :orders, only: [:show, :create] do
+    resources :payments, only: :new
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
